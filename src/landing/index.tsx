a6395c725d2efe1b9c7ee1ee83635afcbe1384cd
sooy <ssy67366736@gmail.com>
2024-09-08 17:49:58 +0900

    feat: login routing

--- src/landing/index.tsx
+++ src/landing/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Landing = ({navigation}: any) => {
+  useEffect(() => {
+    getAccessToken();
+  }, []);
+
+  const getAccessToken = async () => {
+    const ACCESS_TOKEN = await AsyncStorage.getItem('ACCESS_TOKEN');
+    if (ACCESS_TOKEN) {
+      navigation.navigate('Register');
+    }
+  };
+
   return (
     <Button
       title="카카오로그인"
