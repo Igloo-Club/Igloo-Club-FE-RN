dc9df3cc6cdceaaed21a0310ffbf9ff4d0040051
7iw8n <jw4319han@naver.com>
2024-09-09 15:33:52 +0900

    feat: 상세 프로필 페이지 api 연동

--- src/common/routing/Router.tsx
+++ src/common/routing/Router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {IdProvider} from '../apis/contexts/useIdContext';
 import Home from '../Home';
 import DetailProfile from '../../detail/DetailProfilePage';
 import Register from '../../register';
@@ -22,25 +21,24 @@ const navTheme = {
 
 export const Router = () => {
   return (
-    <IdProvider>
-      <NavigationContainer theme={navTheme}>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen
-            name="DetailProfile"
-            component={DetailProfile}
-            options={{title: '상세 프로필 등록'}}
-          />
-          <Stack.Screen
-            name="QnA"
-            component={QnA}
-            options={{title: '추가 답변 등록'}}
-          />
-          <Stack.Screen
-            name="AnswerPage"
-            component={AnswerPage}
-            options={{title: '답변 작성'}}
-          />
-          {/* <Stack.Screen
+    <NavigationContainer theme={navTheme}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="QnA"
+          component={QnA}
+          options={{title: '추가 답변 등록'}}
+        />
+        <Stack.Screen
+          name="DetailProfile"
+          component={DetailProfile}
+          options={{title: '상세 프로필 등록'}}
+        />
+        <Stack.Screen
+          name="AnswerPage"
+          component={AnswerPage}
+          options={{title: '답변 작성'}}
+        />
+        {/* <Stack.Screen
           name="Regitser"
           component={Register}
           options={{title: '필수 프로필 등록'}}
@@ -50,18 +48,17 @@ export const Router = () => {
           component={Home}
           options={{title: 'Overview'}}
         /> */}
-          <Stack.Screen
-            name="IdealType"
-            component={IdealType}
-            options={{title: '이상형 등록'}}
-          />
-          <Stack.Screen
-            name="QuestionList"
-            component={QuestionList}
-            options={{title: '질문 리스트'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </IdProvider>
+        <Stack.Screen
+          name="IdealType"
+          component={IdealType}
+          options={{title: '이상형 등록'}}
+        />
+        <Stack.Screen
+          name="QuestionList"
+          component={QuestionList}
+          options={{title: '질문 리스트'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
