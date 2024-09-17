8fe0f894f9886d7ccd2a3412db979989624e813a
sooy <ssy67366736@gmail.com>
2024-09-17 21:26:33 +0900

    feat: instance

--- src/common/apis/axiosInstance.ts
+++ src/common/apis/axiosInstance.ts
@@ -4,10 +4,10 @@ import axios, {
   InternalAxiosRequestConfig,
 } from 'axios';
 import getRefreshToken from './getRefreshToken';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {VITE_BASE_URL} from '@env';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from './types';
+import getAccessToken from '../utils/getAccessToken';
 
 type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Login'>;
 
@@ -26,13 +26,7 @@ export default instance;
 
 instance.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
-    const ACCESS_TOKEN = await AsyncStorage.getItem('ACCESS_TOKEN');
-
-    // if (!ACCESS_TOKEN) {
-    //   navigation.navigate('Login');
-
-    //   return config;
-    // }
+    const ACCESS_TOKEN = await getAccessToken();
 
     if (config.headers && ACCESS_TOKEN !== null) {
       const token = JSON.parse(ACCESS_TOKEN);
@@ -52,19 +46,11 @@ export const setAxiosInterceptors = (navigation: NavigationProp) => {
     (response: AxiosResponse) => {
       return response;
     },
-    async (error: AxiosError) => {
-      const originalRequest = error.config as InternalAxiosRequestConfig & {
-        _retry?: boolean;
-      };
-
+    async (error: any) => {
+      console.log('@', error);
       // 401 에러 처리
-      if (error.response?.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const isRefreshSuccessful = await getRefreshToken(navigation);
-
-        if (isRefreshSuccessful) {
-          return instance(originalRequest);
-        }
+      if (error.response?.status === 401) {
+        await getRefreshToken(navigation);
       }
 
       return Promise.reject(error);
