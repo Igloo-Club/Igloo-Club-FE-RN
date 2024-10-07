ad751334092eef52f1491a666ea55f5944274743
sooy <ssy67366736@gmail.com>
2024-10-08 00:03:05 +0900

    feat: chat bubble

---
+++ src/chat/components/ChatSpeechBubble.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from '@emotion/native';
+import {formatAMPM} from '../utils/formatAMPM';
+import {Text, View} from 'react-native';
+
+const isSender = true;
+const createdAt = '2024-10-07T14:33:12.421Z';
+
+const ChatSppechBubble = () => {
+  return (
+    <StBubbleContainer isSender={isSender}>
+      {isSender && <StTime>{formatAMPM(createdAt)}</StTime>}
+      <StBubble isSender={isSender}>
+        <Text>ㅎㅇㅎㅇ</Text>
+      </StBubble>
+      {isSender || <StTime>{formatAMPM(createdAt)}</StTime>}
+    </StBubbleContainer>
+  );
+};
+
+export default ChatSppechBubble;
+
+const StBubbleContainer = styled(View)<{isSender: boolean}>`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  align-items: flex-end;
+  align-self: ${({isSender}) => (isSender ? 'flex-end' : 'flex-start')};
+`;
+
+const StTime = styled(Text)`
+  ${({theme}) => theme.fonts.caption};
+  color: ${({theme}) => theme.colors.gray6};
+`;
+
+const StBubble = styled(View)<{isSender: boolean}>`
+  width: fit-content;
+  max-width: 250px;
+  padding: 12px;
+  margin-left: ${({isSender}) => (isSender ? '0' : '7px')};
+  color: ${({theme, isSender}) =>
+    isSender ? theme.colors.white : theme.colors.black};
+  background-color: ${({theme, isSender}) =>
+    isSender ? theme.colors.primary : '#F2F2F2'};
+  border-radius: 17px;
+
+  ${({theme}) => theme.fonts.body1r};
+`;
