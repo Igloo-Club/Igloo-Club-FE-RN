01b4ae8f5bd12f660cb507aab18420c4b9ac264c
7iw8n <jw4319han@naver.com>
2025-01-07 16:27:19 +0900

    feat: 메인 UI 세부 css 작업

---
+++ src/main/components/PickProfileBtn.tsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import styled from '@emotion/native';
+import instance from '../../common/apis/axiosInstanse';
+import {TouchableOpacity, View} from 'react-native';
+import CountDown from './CountDown';
+// import LimitModal from './LimitModal';
+// import ExistModal from './ExistModal';
+
+const PickProfileBtn = (
+  {
+    /*{ ProfileData }: any*/
+  },
+) => {
+  const [activePick, setActivePick] = useState(false);
+  const [isLimitModalOpen, setIsLimitModalOpen] = useState<boolean>(false);
+  const [isExistModalOpen, setIsExistModalOpen] = useState<boolean>(false);
+
+  const handleClickBtn = async () => {
+    //     try {
+    //       const res = await instance.post('/api/nungil/recommend', {
+    //         isPayed: true,
+    //       });
+    //       if (res.data) {
+    //         const { companyName, job, description } = res.data;
+    //         ProfileData({ companyName, job, description });
+    //       } else {
+    //         setIsExistModalOpen(true);
+    //       }
+    //     } catch (error: any) {
+    //       console.log(error);
+    //       if (error.response && error.response.status === 403) {
+    //         setIsLimitModalOpen(true);
+    //       }
+    //     }
+  };
+
+  return (
+    <Container>
+      <PickBtn onPress={handleClickBtn}>
+        {activePick ? (
+          <ActiveMent>번의 추가 프로필 뽑기 기회가 생겼어요!</ActiveMent>
+        ) : (
+          <>
+            <UnActiveMent>
+              다음 추가 프로필 뽑기까지 <CountDown /> 남았어요
+            </UnActiveMent>
+          </>
+        )}
+      </PickBtn>
+      {/* {isLimitModalOpen && (
+        <LimitModal closeModal={() => setIsLimitModalOpen(false)} />
+      )}
+      {isExistModalOpen && (
+        <ExistModal closeModal={() => setIsExistModalOpen(false)} />
+      )} */}
+    </Container>
+  );
+};
+
+export default PickProfileBtn;
+
+const Container = styled(View)`
+  display: flex;
+  width: 100%;
+`;
+
+const PickBtn = styled(TouchableOpacity)<{activePick: boolean}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 0px;
+  background-color: ${({activePick}) => (activePick ? '#FA7268' : '#E4E8EC')};
+`;
+
+const ActiveMent = styled.Text`
+  color: #ffffff;
+  font-size: 15px;
+  font-weight: 400;
+`;
+
+const UnActiveMent = styled.Text`
+  color: #bbc0ca;
+  font-size: 15px;
+  font-weight: 600;
+`;
