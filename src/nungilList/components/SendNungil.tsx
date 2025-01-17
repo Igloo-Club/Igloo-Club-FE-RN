dbe1c6b113c138d14428380bc1f83bf73c1326de
7iw8n <jw4319han@naver.com>
2025-01-17 20:48:51 +0900

    feat: 눈길 리스트 페이지 top nav tab 틀 구현

--- src/nungilList/components/SendNungil.tsx
+++ src/nungilList/components/SendNungil.tsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import styled from '@emotion/native';
+import {View, Text} from 'react-native';
+
 const SendNungil = () => {
-  return <></>;
+  return (
+    <Container>
+      <Text>보낸 눈길</Text>
+    </Container>
+  );
 };
 
 export default SendNungil;
+
+const Container = styled(View)`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  padding: 0px 20px 0px 20px;
+`;
--- src/nungilList/components/SoonNungil.tsx
+++ src/nungilList/components/SoonNungil.tsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import styled from '@emotion/native';
+import {View, Text} from 'react-native';
+
 const SoonNungil = () => {
-  return <></>;
+  return (
+    <Container>
+      <Text>곧 맺어질 눈길</Text>
+    </Container>
+  );
 };
 
 export default SoonNungil;
+
+const Container = styled(View)`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  padding: 0px 20px 0px 20px;
+`;
