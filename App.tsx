a98af38c1af8d0d4d66a0866ccb1a3b2cdd69a36
sooy <ssy67366736@gmail.com>
2025-01-20 19:48:38 +0900

    feat: splash screen

--- App.tsx
+++ App.tsx
@@ -3,8 +3,15 @@ import {Router} from './src/common/routing/Router';
 import {ThemeProvider} from '@emotion/react';
 import {theme} from './src/common/styles/theme';
 import {IdProvider} from './src/common/apis/contexts/useIdContext';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  React.useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500); // 예시: 1.5초 후 스플래시 화면 숨기기
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <IdProvider>
